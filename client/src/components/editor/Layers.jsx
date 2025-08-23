0abfdc60928f302cea6e4469d57e50e948835eac
kashish <kashishchadha0509@gmail.com>
2025-08-23 21:10:32 +0530

    stalk kerke acha lagta hai kya??

--- client/src/components/editor/Layers.jsx
+++ client/src/components/editor/Layers.jsx
@@ -1,17 +1,22 @@
+import useEditorStore from '../../utils/editorStore.js'
 import Image from '../image/image.jsx'
 const Layers=()=>{
+const {selectedLayer, setSelectedLayer}=useEditorStore()
+const handleSelectedLayer=(layer)=>{
+setSelectedLayer(layer);
+}
     return(<><div className="layers">
         <div className="layersTitle">
             <h3>Layers</h3>
             <p>Select a layer to edit</p>
         </div>
-        <div className="layer">
+        <div  onClick={()=>handleSelectedLayer("text")} className={`layer ${selectedLayer==="text"?"selected":""}`}>
             <div className="layerImage">
                 <Image path="/general/text.png " alt='' w={48} h={48}/>
             </div>
             <span>Add Text</span>
         </div>
-        <div className="layer">
+        <div onClick={()=>handleSelectedLayer("canvas")} className={`layer ${selectedLayer==="canvas"?"selected":""}`}>
             <div className="layerImage" style={{backgroundColor:"teal"}}></div>
             <span>Canvas</span>
 
--- client/src/utils/editorStore.js
+++ client/src/utils/editorStore.js
@@ -2,7 +2,5 @@ import { create } from 'zustand'
 const useEditorStore = create((set) => ({
   selectedLayer:"canvas" ,
   setSelectedLayer: (newLayer) => set({ selectedLayer:newLayer }),
-removeCurrentUser: () => set({ currentUser: null }),
-  updateCurrentUser: (updatedUser) => set({ currentUser: updatedUser }),
 }));
 export default useEditorStore
