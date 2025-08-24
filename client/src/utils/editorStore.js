470fffcef7b50a1284a32d0ebfca2dea0cc2712b
kashish <kashishchadha0509@gmail.com>
2025-08-24 22:57:57 +0530

    checking again

--- client/src/utils/editorStore.js
+++ client/src/utils/editorStore.js
@@ -1,21 +1,51 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+
 const useEditorStore = create((set) => ({
-  selectedLayer:"canvas" ,
-  textOptions:{
-    text:"",
-    fontSize:48,
-    color:"#000000",
-    top:0,
-    left:0,
+  selectedLayer: "canvas",
+  textOptions: {
+    text: "",
+    fontSize: 48,
+    color: "#000000",
+    top: 48,
+    left: 0,
   },
-  setSelectedLayer: (newLayer) => set({ selectedLayer:newLayer }),
-  setTextOptions:(newLayer)=>set({selectedLayer:newLayer}),
-  addText:()=>set({textOptions:{
-     text:"Add Text",
-    fontSize:48,
-    color:"#000000",
-    top:0,
-    left:0,
-  }})
+  canvasOptions: {
+    height: 0,
+    orientation: "",
+    size: "original",
+    backgroundColor: "#008080",
+  },
+  setSelectedLayer: (newLayer) => set({ selectedLayer: newLayer }),
+  setTextOptions: (newOptions) => set({ textOptions: newOptions }),
+  addText: () =>
+    set({
+      textOptions: {
+        text: "Add text",
+        fontSize: 48,
+        color: "#000000",
+        top: 48,
+        left: 0,
+      },
+    }),
+  setCanvasOptions: (newOption) => set({ canvasOptions: newOption }),
+  // FIXED: ADD RESET FUNCTION
+  resetStore: () =>
+    set({
+      selectedLayer: "canvas",
+      textOptions: {
+        text: "",
+        fontSize: 48,
+        color: "#000000",
+        top: 48,
+        left: 0,
+      },
+      canvasOptions: {
+        height: 0,
+        orientation: "",
+        size: "original",
+        backgroundColor: "#008080",
+      },
+    }),
 }));
-export default useEditorStore
+
+export default useEditorStore;
